575bf92767d8ab7d2107c3fb492b27e7fcf2bf68
Adilson Pavan <adilson.pavan@gmail.com>
2022-02-25 23:57:28 -0300

    Lint and convert code to python3

--- emec/__init__.py
+++ emec/__init__.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import emec
-from emec import *
+__author__ = "Adilson Pavan"
 
-__author__ = 'Adilson Pavan'
+VERSION = (0, 1, 6)
 
-VERSION = (0,1,6)
 
 def get_version():
-    version = '%s.%s' % (VERSION[0], VERSION[1])
+    version = f"{VERSION[0]}.{VERSION[1]}"
     if VERSION[2]:
-        version = '%s.%s' % (version, VERSION[2])
+        version = f"{version}.{VERSION[2]}"
     return version
 
+
 __version__ = get_version()
-
-
--- emec/utils.py
+++ emec/utils.py
@@ -2,21 +2,17 @@
 
 from unicodedata import normalize
 
-def normalize_key(key):
-    """
-    Formata a chave para ser utilizada no json.
-    
+
+def normalize_key(key: str) -> str:
+    """Formata a chave para ser utilizada no json.
+
     Args:
-        key (string):    Campo coletado no scraping dos dados do MEC.
-        
+        key (str): Campo coletado no scraping dos dados do MEC.
+
     Returns:
-        Retorna a sttring formatada para ser utilizada no json.
+        str: Retorna a sttring formatada para ser utilizada no json.
     """
-    
-    aux = key.strip(' :').replace(' ', '_').lower()
-    aux = aux.replace('_-_sigla', '')
-    
-    return normalize('NFKD', aux).encode('ASCII','ignore')
+    aux = key.strip(" :").replace(" ", "_").lower()
+    aux = aux.replace("_-_sigla", "")
 
-
-    
+    return normalize("NFKD", aux).encode("ASCII", "ignore")
