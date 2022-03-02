4bff08fc636d88abf9c8ee61e46b535858f49dad
Adilson Pavan <adilson.pavan@gmail.com>
2022-03-01 23:36:34 -0300

    creating structure for emec cli

---
+++ emec/application.py
@@ -0,0 +1,22 @@
+from cleo import Application as BaseApplication
+
+from .commands.about import AboutCommand
+
+try:
+    from emec.__version__ import __version__
+except ImportError:
+    from __version__ import __version__
+
+
+class Application(BaseApplication):
+    def __init__(self):
+        super(Application, self).__init__("emec", __version__)
+
+        for command in self.get_default_commands():
+            self.add(command)
+
+    def get_default_commands(self) -> list:
+        commands = [
+            AboutCommand(),
+        ]
+        return commands
---
+++ emec/commands/about.py
@@ -0,0 +1,15 @@
+from cleo import Command
+from pyfiglet import Figlet
+
+
+class AboutCommand(Command):
+    name = "about"
+    description = "Shows information about emec api"
+
+    def handle(self):
+        custom_fig = Figlet(font="big")
+        title = custom_fig.renderText("emec api")
+        self.line(
+            f"{title}\n<info>This tool was developed to facilitate"
+            f" the use of the emec api app.</info>"
+        )
