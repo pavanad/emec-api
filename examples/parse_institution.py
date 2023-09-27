c08e1157abf126d97edbb4da8e4b28c29a92d134
Adilson Pavan <adilson.pavan@zup.com.br>
2023-09-27 15:12:51 -0300

    adicionando main no exemplo

--- examples/parse_institution.py
+++ examples/parse_institution.py
@@ -4,23 +4,28 @@ import time
 from emec.api.client import Institution
 
 
-start = time.time()
-ies = Institution(22)
-ies.parse()
-elapsed = time.time() - start
-print(f"Elapsed: {elapsed}")
+def main():
+    start = time.time()
+    ies = Institution(22)
+    ies.parse()
+    elapsed = time.time() - start
+    print(f"Elapsed: {elapsed}")
 
-df_inst = ies.get_institution_dataframe()
-print(df_inst.head())
+    df_inst = ies.get_institution_dataframe()
+    print(df_inst.head())
 
-df_campus = ies.get_campus_dataframe()
-print(df_campus.head())
+    df_campus = ies.get_campus_dataframe()
+    print(df_campus.head())
 
-df_courses = ies.get_courses_dataframe()
-print(df_courses.head())
+    df_courses = ies.get_courses_dataframe()
+    print(df_courses.head())
 
-# export to json
-ies.to_json("emec.json")
+    # export to json
+    ies.to_json("emec.json")
 
-# export to csv
-ies.to_csv("emec.csv")
+    # export to csv
+    ies.to_csv("emec.csv")
+
+
+if __name__ == "__main__":
+    main()
